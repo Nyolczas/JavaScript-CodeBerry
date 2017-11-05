52871722b325a629ec08f74f5e5f4140c9f8728b
Nyolczas <nyolczas@gmail.com>
2017-11-05 18:21:55 +0100

    tetractys shorter

--- tetractys.js
+++ tetractys.js
@@ -1,35 +1,35 @@
 var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-var canvasWidth = context.canvas.clientWidth;
-var canvasHeight = context.canvas.clientHeight;
+var cnw = ctx.canvas.clientWidth;
+var cnh = ctx.canvas.clientHeight;
 
-var cellWidth = 100;
-var halfW = Math.round(cellWidth * 0.5);
-var cellHeight = Math.sin(60 * Math.PI / 180) * cellWidth;
-var tetrArray = [
-    [1, cellWidth, 'rgb(227, 98, 102)', cellWidth, cellHeight],
-    [2, halfW, 'rgb(38, 173, 73)', 0, cellHeight],
-    [3, 0, 'rgb(34, 129, 128)', halfW, 0]
+var bxw = 100;
+var bxw2 = Math.round(bxw * 0.5);
+var bxh = Math.sin(60 * Math.PI / 180) * bxw;
+var trA = [
+    [1, bxw, 'rgb(227, 98, 102)', bxw, bxh],
+    [2, bxw2, 'rgb(38, 173, 73)', 0, bxh],
+    [3, 0, 'rgb(34, 129, 128)', bxw2, 0]
 ];
-var xShft = Math.round((canvasWidth - cellWidth * 3) / 2);
-var yShft = Math.round((canvasHeight - cellHeight * 3) / 2);
+var xShft = Math.round((cnw - bxw * 3) / 2);
+var yShft = Math.round((cnh - bxh * 3) / 2);
 
 for (var h = 0; h < 3; h++) {
-    context.fillStyle = tetrArray[h][2];
-    for (var w = 0; w < tetrArray[h][0]; w++) {
-        triangle(xShft + tetrArray[h][1] + (cellWidth * w),
-        Math.round(yShft + (h * cellHeight)),
-        tetrArray[h][2]);
+    ctx.fillStyle = trA[h][2];
+    for (var w = 0; w < trA[h][0]; w++) {
+        triangle(xShft + trA[h][1] + (bxw * w),
+        Math.round(yShft + (h * bxh)),
+        trA[h][2]);
     }
 }
 
 function triangle(x,y,clr) {
-    context.beginPath();
-    context.moveTo(x + halfW, y);
+    ctx.beginPath();
+    ctx.moveTo(x + bxw2, y);
     for (i = 0; i < 3; i++) {
-        context.lineTo(x + tetrArray[i][3], y + tetrArray[i][4]);
+        ctx.lineTo(x + trA[i][3], y + trA[i][4]);
     }
-    context.fillStyle = clr;
-    context.fill();
+    ctx.fillStyle = clr;
+    ctx.fill();
 }
