fcf336e39bcb3954fe212ee51338e77191314a2b
Nyolczas <nyolczas@gmail.com>
2017-11-05 16:24:36 +0100

    tetractiys

---
+++ tetractys.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+var canvasWidth = context.canvas.clientWidth;
+var canvasHeight = context.canvas.clientHeight;
